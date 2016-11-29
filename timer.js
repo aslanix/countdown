b3dbcb75a130261c49a14cc9a763342d5421fb69
Aslan Askarov <aslan@askarov.net>
2016-11-29 22:14:12 +0100

    checkpoint

--- timer.js
+++ timer.js
@@ -1,6 +1,40 @@
-let duration_min = 30;
+let duration_min;
+let when;
 
-let when = moment().add (duration_min, 'minutes');
+
+function showLog( ) {
+  let l_ = localStorage.getItem("logMessages");
+  document.getElementById("log").innerHTML = l_;
+}
+
+
+function log_ (y) {
+  let x = moment().format() + ': ' + y;
+
+  // console.log (x);
+
+  let l_ = localStorage.getItem("logMessages");
+  l_ = "<p>" + x + "</p>" + l_;
+
+  localStorage.setItem("logMessages", l_);
+  document.getElementById("log").innerHTML = l_;
+}
+
+
+function reset(x)  {
+  if (x < 1) {
+    duration_min = 0;
+    when = moment().add(x*100, 'seconds');
+  } else {
+    duration_min = x
+    when = moment().add(duration_min,'minutes');
+  }
+
+  localStorage.setItem("duration_min", duration_min);
+  localStorage.setItem("when", when.unix());
+  log_ ( "duration set to " + x + " min until " + when.format() );
+}
+
 
 function format_2 (x) {
   let y = Math.floor(x);
@@ -28,16 +62,52 @@ function timer(where) {
 
     setTimeout(timer, 1000, where);
   } else {
-    where.innerHTML = "[Timer done!]";
+    where.innerHTML = "[TIMER DONE]";
+    let audio = new Audio('media/ZenTempleBell.mp3');
+    audio.play();
   }
 }
 
 
-function startTimer() {
-  timer (document.getElementById("counter") );
+function first(x) {
+  log_ ("First run at " + moment().format());
+  reset (x);
 }
 
-function reset(x)  {
-  duration_min = x
-  when = moment().add(duration_min,'minutes');
+
+function clearlog () {
+
+  localStorage.setItem("logMessages", "");
+
+  document.getElementById("log").innerHTML = "";
+}
+
+
+function startTimer() {
+  let run_first = false;
+  try {
+    let when_ = moment.unix(localStorage.getItem("when"));
+    let now_ = moment();
+
+    if (now_.isBefore(when_)) {
+        when = when_;
+
+    } else {
+      run_first = true;
+    }
+
+  } catch (err) {
+    console.log(err);
+    run_first = true;
+  }
+
+
+  if (run_first) {
+    first(30);
+  } else {
+    showLog ()
+  }
+
+  timer(document.getElementById("counter"));
+
 }
